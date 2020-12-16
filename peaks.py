00b4e122f5527960a56659b25c37d8533fce05a9
ev-an-w <ewgm1234@gmail.com>
2020-12-16 10:45:40 -0800

    small formatting edits

--- peaks.py
+++ peaks.py
@@ -25,7 +25,7 @@ for i in range(len(dataPoints)):
     # Criteria for a peak:
     # 110% above the average of its containing chunk
     # Greater than each point on the left and right
-    if (dataPoints[i] >= 1.1 * avg and dataPoints[i] >= dataPoints[i - 1]
+    if (dataPoints[i] >= 1.1*avg and dataPoints[i] >= dataPoints[i - 1]
             and dataPoints[i] >= dataPoints[i + 1]):
         peaks.append(dataPoints[i])
 
